895d61fb7b7c8b69d4926b3a3de0b271952c22ad
JesusGuerrero <rocha.jesus@hotmail.com>
2015-06-15 22:06:48 -0700

    Init setup for Heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
-require('node-env-file')('.env');
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.NODE_PORT = process.env.NODE_PORT || '8042';
+process.env.NODE_URL = process.env.NODE_URL || '127.0.0.1';
+
+if (process.env.NODE_ENV === 'development') {
+  require('node-env-file')('.env');
+  console.log('Running Development!');
+}
 
 var config = require('./config/environments/variables.js')(process.env),
     server = require('./config/server/hapi.js')(config, process.env.NODE_URL );
