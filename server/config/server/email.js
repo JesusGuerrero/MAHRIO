02d2b71641009afc49a2f644b7c81ca6fa86f824
JesusGuerrero <rocha.jesus@hotmail.com>
2015-08-03 08:02:29 -0700

    Refactor - contact api

--- server/config/server/email.js
+++ server/config/server/email.js
@@ -2,13 +2,13 @@
 
 //var nodemailer = require('nodemailer');
 var Mailgun = require('mailgun-js');
-var from_who = 'doNotReply@whichdegree.co';
+var fromWho = 'doNotReply@whichdegree.co';
 
 module.exports = function( mailObj ) {
   var mailgun = new Mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
 
   var data = {
-    from: from_who,
+    from: fromWho,
     to: mailObj.to,
     subject: mailObj.text,
     html: mailObj.html
@@ -18,7 +18,7 @@ module.exports = function( mailObj ) {
   mailgun.messages().send(data, function (err, body) {
     //If there is an error, render the error page
     if (err) {
-      console.log("got an error: ", err);
+      console.log('got an error: ', err);
     } else {
       console.log(body);
     }
