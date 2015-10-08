593784eea421ed26a8e6b1d003316551ec0b1e50
EMCP <emcp@whichdegree.co>
2015-10-07 22:27:29 -0700

    Initial upload of Test Suites for E2E

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,236 +1,283 @@
 'use strict';
 
 module.exports = function (grunt) {
-  
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-    pkg: grunt.file.readJSON('package.json'),
-    newer: {
-      options: {
-        override: function(detail, include) {
-          include( false );
-        }
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        '*.js',
-        'client/**/*.js',
-        'server/**/*.js',
-        'test/**/*.js',
-        '!server/public/**/*.js'
-      ]
-    },
+    grunt.initConfig({
 
-    less: {
-      development: {
-        options: {
-          //compress: true  //minifying the result
+        pkg: grunt.file.readJSON('package.json'),
+        newer: {
+            options: {
+                override: function(detail, include) {
+                    include( false );
+                }
+            }
         },
-        files: {
-          'server/public/css/application.css' : 'client/styles/application.less'
-        }
-      }
-    },
-
-    jade: {
-      compile: {
-        options: {
-          client: false,
-          pretty: true
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                '*.js',
+                'client/**/*.js',
+                'server/**/*.js',
+                'test/**/*.js',
+                '!server/public/**/*.js'
+            ]
         },
-        files: [{
-          cwd: 'client/angular/',
-          src: ['**/*.jade'],
-          dest: 'server/public/html',
-          expand: true,
-          ext: '.html'
-        }]
-      }
-    },
 
-    concat: {
-      options: {
-        separator: '\n'
-      },
-      angular: {
-        src: [
-          'bower_components/jquery/dist/jquery.js',
-          'bower_components/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.all.js',
-          'bower_components/jquery-ui/jquery-ui.js',
-          'bower_components/underscore/underscore.js',
-          'bower_components/socket.io-client/socket.io.js',
-          'bower_components/moment/moment.js',
-          'bower_components/fullcalendar/dist/fullcalendar.js',
-          'bower_components/bootstrap/dist/js/bootstrap.js',
-          'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
-          'bower_components/jquery.slimscroll/jquery.slimscroll.min.js',
-          'bower_components/fastclick/lib/fastclick.js',
-          'bower_components/icheck/icheck.min.js',
-          'bower_components/Chart.js/Chart.js',
-          'bower_components/angular/angular.js',
-          'bower_components/angular-ui-router/release/angular-ui-router.js',
-          'bower_components/angular-ui-sortable/sortable.min.js',
-          'bower_components/angular-ui-bootstrap/src/modal/modal.js',
-          'bower_components/angular-ui-bootstrap/src/typeahead/typeahead.js',
-          'bower_components/angular-ui-bootstrap/src/position/position.js',
-          'bower_components/angular-ui-bootstrap/src/bindHtml/bindHtml.js',
-          'bower_components/angular-ui-bootstrap/src/tabs/tabs.js',
-          'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-          'bower_components/angular-resource/angular-resource.js',
-          'bower_components/angular-route/angular-route.js',
-          'bower_components/angular-cookies/angular-cookies.js',
-          'bower_components/angular-sanitize/angular-sanitize.js',
-          'bower_components/angular-animate/angular-animate.js',
-          'bower_components/angular-touch/angular-touch.js',
-          'bower_components/angular-loading-bar/build/loading-bar.js',
-          'bower_components/angular-underscore/angular-underscore.min.js',
-          'bower_components/angular-socket-io/socket.js',
-          'bower_components/angular-chart.js/dist/angular-chart.js',
-          'bower_components/matchHeight/jquery.matchHeight-min.js',
-          'client/AdminLTE/AdminLTE.js'
-        ],
-        dest: 'server/public/js/angular.js'
-      },
-      utils: {
-          src: [
-              'bower_components/jquery/dist/jquery.js',
-              'bower_components/jquery.videoBG/jquery.videoBG.js',
-              'bower_components/bootstrap/dist/js/bootstrap.js',
-              'bower_components/requirejs/require.js',
-              'client/requirejs/setup.js'
-          ],
-          dest: 'server/public/js/utils.js'
-      },
-      application: {
-        src: [
-          'server/public/js/angular.js',
-          'client/AdminLTE/AdminLTE.js',
-          'client/angular/application.js',
-          'client/angular/**/*.js'
-        ],
-        dest: 'server/public/js/application.js'
-      }
-    },
-
-    karma: {
-      once: {
-        configFile: 'test/karma.conf.js',
-        singleRun: true
-      },
-      watch: {
-        configFile: 'test/karma.conf.js'
-      }
-    },
-
-    exec: {
-      npmTest: 'npm test'
-    },
-
-    copy: {
-      build: {
-        cwd: 'bower_components',
-        src: [ 'fontawesome/fonts/*.{otf,eot,svg,ttf,woff,woff2}', 'bootstrap/fonts/*.{otf,eot,svg,ttf,woff,woff2}'],
-        dest: 'server/public/fonts',
-        expand: true,
-        flatten: true
-      },
-      jade: {
-        cwd: 'client/views',
-        src: ['**/_*.jade'],
-        dest: 'server/views',
-        expand: true
-      },
-      html: {
-        cwd: 'client/angular',
-        src: ['**/*.html'],
-        dest: 'server/public/html',
-        expand: true
-      },
-      dev: {
-        cwd: 'client/angular',
-        src: ['**/*.js'],
-        dest: 'server/public/js/angular',
-        expand: true
-      },
-      devScripts: {
-        src: 'server/public/dev-scripts.jade',
-        dest: 'server/views/common/_scripts.jade'
-      },
-      requireJs: {
-        cwd: 'client/requirejs',
-        src: ['**/*.js', '!setup.js'],
-        dest: 'server/public/require',
-        expand: true
-      }
-    },
-    watch: {
-      less: {
-        files: ['client/styles/**/*.less','client/angular/**/*.less'],
-        tasks: ['less']
-      },
-      angularJade: {
-        files: ['client/angular/**/*.jade', '!**/_*.jade'],
-        tasks: ['newer:jade'],
-        options: {
-          interrupt: true
-        }
-      },
-      serverJade: {
-        files: ['client/views/**/_*.jade'],
-        tasks: ['newer:jade']
-      },
-      scriptsClient: {
-        files: ['client/angular/**/*.js'],
-        tasks: ['newer:copy:dev','newer:jshint:all'],
-        options: {
-          interrupt: true
-        }
-      },
-      scriptsServer: {
-        files: [
-          'server/**/*.js',
-          '!server/public/**/*.js',
-          'test/**/*.js'
-        ],
-        tasks: ['newer:jshint:all']
-      },
-      scriptsGrunt: {
-        files: [
-          '*.js'
-        ],
-        tasks: ['newer:jshint:all']
-      },
-      scriptsTests: {
-        files: [
-          'test/**/*.js'
-        ],
-        tasks: ['jshint:all']
-      }
-    },
-    tags: {
-      build: {
-        options: {
-          scriptTemplate: 'script( src="/assets/{{ path }}")',
-          openTag: '<!-- start template tags -->',
-          closeTag: '<!-- end template tags -->'
+        less: {
+            development: {
+                options: {
+                    //compress: true  //minifying the result
+                },
+                files: {
+                    'server/public/css/application.css' : 'client/styles/application.less'
+                }
+            }
         },
-        src: [
-          'server/public/js/angular/application.js',
-          'server/public/js/angular/**/*.js'
-        ],
-        dest: 'server/public/dev-scripts.jade'
-      }
-    }
-  });
 
-  grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['less', 'jade', 'concat', 'copy']);
-  grunt.registerTask('build-dev', ['less', 'jade', 'tags', 'copy:dev', 'copy:devScripts', 'concat:angular']);
+        jade: {
+            compile: {
+                options: {
+                    client: false,
+                    pretty: true
+                },
+                files: [{
+                    cwd: 'client/angular/',
+                    src: ['**/*.jade'],
+                    dest: 'server/public/html',
+                    expand: true,
+                    ext: '.html'
+                }]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: '\n'
+            },
+            angular: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/bootstrap3-wysihtml5-bower/dist/bootstrap3-wysihtml5.all.js',
+                    'bower_components/jquery-ui/jquery-ui.js',
+                    'bower_components/underscore/underscore.js',
+                    'bower_components/socket.io-client/socket.io.js',
+                    'bower_components/moment/moment.js',
+                    'bower_components/fullcalendar/dist/fullcalendar.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+                    'bower_components/jquery.slimscroll/jquery.slimscroll.min.js',
+                    'bower_components/fastclick/lib/fastclick.js',
+                    'bower_components/icheck/icheck.min.js',
+                    'bower_components/Chart.js/Chart.js',
+                    'bower_components/angular/angular.js',
+                    'bower_components/angular-ui-router/release/angular-ui-router.js',
+                    'bower_components/angular-ui-sortable/sortable.min.js',
+                    'bower_components/angular-ui-bootstrap/src/modal/modal.js',
+                    'bower_components/angular-ui-bootstrap/src/typeahead/typeahead.js',
+                    'bower_components/angular-ui-bootstrap/src/position/position.js',
+                    'bower_components/angular-ui-bootstrap/src/bindHtml/bindHtml.js',
+                    'bower_components/angular-ui-bootstrap/src/tabs/tabs.js',
+                    'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                    'bower_components/angular-resource/angular-resource.js',
+                    'bower_components/angular-route/angular-route.js',
+                    'bower_components/angular-cookies/angular-cookies.js',
+                    'bower_components/angular-sanitize/angular-sanitize.js',
+                    'bower_components/angular-animate/angular-animate.js',
+                    'bower_components/angular-touch/angular-touch.js',
+                    'bower_components/angular-loading-bar/build/loading-bar.js',
+                    'bower_components/angular-underscore/angular-underscore.min.js',
+                    'bower_components/angular-socket-io/socket.js',
+                    'bower_components/angular-chart.js/dist/angular-chart.js',
+                    'bower_components/matchHeight/jquery.matchHeight-min.js',
+                    'client/AdminLTE/AdminLTE.js'
+                ],
+                dest: 'server/public/js/angular.js'
+            },
+            utils: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/jquery.videoBG/jquery.videoBG.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.js',
+                    'bower_components/requirejs/require.js',
+                    'client/requirejs/setup.js'
+                ],
+                dest: 'server/public/js/utils.js'
+            },
+            application: {
+                src: [
+                    'server/public/js/angular.js',
+                    'client/AdminLTE/AdminLTE.js',
+                    'client/angular/application.js',
+                    'client/angular/**/*.js'
+                ],
+                dest: 'server/public/js/application.js'
+            }
+        },
+
+        connect: {
+            options: {
+                port: 9000,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    livereload: 35729,
+                    open: true,
+                    base: ['server']
+
+                }
+            },
+            test: {
+                options: {
+                    base: ['server']
+                }
+            }
+        },
+
+        karma: {
+            once: {
+                configFile: 'test/karma.conf.js',
+                singleRun: true
+            },
+            watch: {
+                configFile: 'test/karma.conf.js'
+            }
+        },
+
+        protractor: {
+            options: {
+                configFile: "test/client.conf.js", // Default config file
+                // keepAlive: true, // If false, the grunt process stops when the test fails.
+                noColor: false, // If true, protractor will not use colors in its output.
+                //debug: true,
+                args: {
+                    verbose: true
+                }
+            },
+            e2e: {
+                options: {
+                    keepAlive: false
+                }
+            },
+            continuous: {
+                options: {
+                    keepAlive: true
+                }
+            }
+        },
+
+        exec: {
+            npmTest: 'npm test'
+        },
+
+        copy: {
+            build: {
+                cwd: 'bower_components',
+                src: [ 'fontawesome/fonts/*.{otf,eot,svg,ttf,woff,woff2}', 'bootstrap/fonts/*.{otf,eot,svg,ttf,woff,woff2}'],
+                dest: 'server/public/fonts',
+                expand: true,
+                flatten: true
+            },
+            jade: {
+                cwd: 'client/views',
+                src: ['**/_*.jade'],
+                dest: 'server/views',
+                expand: true
+            },
+            html: {
+                cwd: 'client/angular',
+                src: ['**/*.html'],
+                dest: 'server/public/html',
+                expand: true
+            },
+            dev: {
+                cwd: 'client/angular',
+                src: ['**/*.js'],
+                dest: 'server/public/js/angular',
+                expand: true
+            },
+            devScripts: {
+                src: 'server/public/dev-scripts.jade',
+                dest: 'server/views/common/_scripts.jade'
+            },
+            requireJs: {
+                cwd: 'client/requirejs',
+                src: ['**/*.js', '!setup.js'],
+                dest: 'server/public/require',
+                expand: true
+            }
+        },
+        watch: {
+            less: {
+                files: ['client/styles/**/*.less','client/angular/**/*.less'],
+                tasks: ['less']
+            },
+            angularJade: {
+                files: ['client/angular/**/*.jade', '!**/_*.jade'],
+                tasks: ['newer:jade'],
+                options: {
+                    interrupt: true
+                }
+            },
+            protractor: {
+                files: ['client/**/*.js', 'test/**/*.js'],
+                tasks: ['protractor:continuous']
+            },
+            serverJade: {
+                files: ['client/views/**/_*.jade'],
+                tasks: ['newer:jade']
+            },
+            scriptsClient: {
+                files: ['client/angular/**/*.js'],
+                tasks: ['newer:copy:dev','newer:jshint:all'],
+                options: {
+                    interrupt: true
+                }
+            },
+            scriptsServer: {
+                files: [
+                    'server/**/*.js',
+                    '!server/public/**/*.js',
+                    'test/**/*.js'
+                ],
+                tasks: ['newer:jshint:all']
+            },
+            scriptsGrunt: {
+                files: [
+                    '*.js'
+                ],
+                tasks: ['newer:jshint:all']
+            },
+            scriptsTests: {
+                files: [
+                    'test/**/*.js'
+                ],
+                tasks: ['jshint:all']
+            }
+        },
+        tags: {
+            build: {
+                options: {
+                    scriptTemplate: 'script( src="/assets/{{ path }}")',
+                    openTag: '<!-- start template tags -->',
+                    closeTag: '<!-- end template tags -->'
+                },
+                src: [
+                    'server/public/js/angular/application.js',
+                    'server/public/js/angular/**/*.js'
+                ],
+                dest: 'server/public/dev-scripts.jade'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less', 'jade', 'concat', 'copy']);
+    grunt.registerTask('build-dev', ['less', 'jade', 'tags', 'copy:dev', 'copy:devScripts', 'concat:angular']);
+    grunt.registerTask('e2e-test', ['connect:test',  'protractor:e2e' ]);
 };
---
+++ test/client/Utils/testUtils.js
@@ -0,0 +1,220 @@
+var fs = require('fs');
+var path = require('path');
+
+var TestUtils = function () {
+    'use strict';
+
+    this.writeToLog = function (message, suiteName, isTitleMessage) {
+
+        if (typeof message !== 'string') {
+            message = "WARNING: messaged was not a string";
+        }
+        var delimiter = '-',
+            d = new Date(),
+            dateString = d.getDate()  + "-" + (d.getMonth() + 1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+        if (isTitleMessage) {
+            message = message.toUpperCase() + '\n';
+        } else {
+            delimiter = '--';
+        }
+        console.log(dateString + " " + suiteName + " " + delimiter + message);
+    };
+
+    this.pad = function (num, size) {
+        var s = "000000000" + num;
+        return s.substr(s.length - size);
+    };
+
+    this.generateScreenshotPathAndFilename = function (metadata) {
+        var testSuiteName = metadata['testSuiteName'];
+        var paddedCounter = metadata['counter'];
+        var message = metadata['message'];
+
+        return browser.params.baseTestFolderName + path.sep + testSuiteName + path.sep + browser.params.browserName + path.sep + paddedCounter + "_" + message + "_" + browser.params.dimensions.width + "x" + browser.params.dimensions.height + ".png"
+    };
+
+    this.findAccordionElement = function (name) {
+        var foundEle = null;
+
+        element.all(by.binding('group.displayName')).each(function(ele) {
+            ele.getText().then(function(txt) {
+                if (txt === name) {
+                    foundEle = ele;
+                }
+            });
+        }, function() {
+            self.writeToLog("ERROR - Stale element, rerunning search! ");
+            element.all(by.binding('group.displayName')).each(function(ele) {
+                ele.getText().then(function(txt) {
+                    if (txt === name) {
+                        foundEle = ele;
+                    }
+                });
+            });
+        });
+
+        return browser.wait(function() {
+            if (foundEle) { return foundEle; }
+        }, 120000);
+
+    };
+
+    this.goBack = function(backButtonCSS) {
+        return element(by.css(backButtonCSS)).click();
+    };
+
+    this.getAccordionElementByCSS = function (name, category) {
+        var self = this;
+        var deviceEle = null;
+        element.all(by.css(category)).each(function (ele) {
+            ele.getText().then(function (txt) {
+                if (txt.indexOf(name) > -1) {
+                    self.writeToLog("Element " + name + " found under accordion " + category, "testUtils.js");
+                    deviceEle = ele;
+                }
+            });
+        });
+
+        return browser.wait(function () {
+            if (deviceEle) {
+                return deviceEle;
+            }
+        }, 120000);
+    };
+
+    this.waitForExpectedElementToBeClickableByCSS = function (expectedElementCSS, timeout) {
+
+        this.writeToLog("waiting for expected element " + expectedElementCSS + " to be Clickable");
+
+        var expectedElement = element(by.css(expectedElementCSS));
+
+        if (timeout === undefined) {
+            timeout = 10000;
+        }
+
+        return this.waitForExpectedElementToBeClickable(expectedElement, timeout);
+    };
+
+    this.waitForExpectedElementToBeVisibleByCSS = function (expectedElementCSS, timeout) {
+
+        this.writeToLog("waiting for expected element " + expectedElementCSS + " to be Visible");
+
+        var expectedElement = element(by.css(expectedElementCSS));
+
+        if (timeout === undefined) {
+            timeout = 10000;
+        }
+
+        return this.waitForExpectedElementToBeVisible(expectedElement, timeout);
+    };
+
+    this.waitForExpectedElementToBeInvisibleByCSS = function (expectedElementCSS, timeout) {
+
+        this.writeToLog("waiting for expected element " + expectedElementCSS + " to be Invisible");
+
+        var expectedElement = element(by.css(expectedElementCSS));
+
+        return this.waitForExpectedElementToBeInvisible(expectedElement, timeout);
+    };
+
+    this.waitForExpectedElementToBeVisible = function (expectedElement, timeout) {
+
+        if (timeout === undefined) {
+            timeout = 10000;
+        }
+
+        var EC = protractor.ExpectedConditions;
+        return browser.wait(EC.visibilityOf(expectedElement), timeout).then(function (element) {
+            return element;
+        });
+    };
+
+    this.waitForExpectedElementToBeClickable = function (expectedElement, timeout) {
+
+        if (timeout === undefined) {
+            timeout = 10000;
+        }
+
+        var EC = protractor.ExpectedConditions;
+        return browser.wait(EC.elementToBeClickable(expectedElement), timeout).then(function (element) {
+            return element;
+        });
+    };
+
+    this.waitForExpectedElementToBeInvisible = function (expectedElement, timeout) {
+
+        if (timeout === undefined) {
+            timeout = 10000;
+        }
+
+        var EC = protractor.ExpectedConditions;
+        return browser.wait(EC.invisibilityOf(expectedElement), timeout).then(
+            function (element) {
+                return element;
+            },
+            function(error){
+                if(error.name === "StaleElementReferenceError"){
+                    console.log("ignoring stale element, it's already invisible");
+                } else if(error.name === 'NoSuchElementError') {
+                    console.log("ignoring element, it's already invisible");
+                } else {
+                    throw error;
+                }
+            });
+    };
+
+    this.scrollIntoViewIfNotVisibleByCSS = function(elementCSS) {
+
+        var targetedElement = element(by.css(elementCSS));
+
+        return this.scrollIntoViewIfNotVisible(targetedElement);
+    };
+
+    this.scrollIntoViewIfNotVisible = function(targetedElement) {
+        var self = this;
+
+        return targetedElement.isDisplayed().then(function(isVisible){
+            if(isVisible){
+                return true;
+            } else {
+                return browser.executeScript(self.scrollIntoView, targetedElement.getWebElement());
+            }
+        });
+    };
+
+    this.scrollIntoView = function() {
+        arguments[0].scrollIntoView();
+    };
+
+    this.clickWithRetryOnError = function (targetElement) {
+        var self = this;
+
+        expect(targetElement.isPresent()).toBeTruthy();
+        expect(targetElement.isDisplayed()).toBeTruthy();
+
+        targetElement.click().then(function(elm){
+            return true;
+        }, function(error){
+            console.log("Error occurred clicking element...");
+            if(error.message.indexOf('Element is not clickable at point') > -1){
+                console.log("scrolling into view now..");
+                return browser.executeScript(self.scrollIntoView, targetElement.getWebElement())
+                    .then(function(element){
+                        console.log("and retrying click");
+                        targetElement.click();
+                    });
+            } else {
+                return error;
+            }
+        });
+    };
+
+    this.clickWithRetryOnErrorByCSS = function (elementCSS) {
+        var targetElement = element(by.css(elementCSS));
+
+        return this.clickWithRetryOnError(targetElement);
+    };
+    
+};
+
+module.exports = new TestUtils();
